62810108491826989719c20fcb8bb1e03aa00c39
truongbvnedu <169114325+truongbvnedu@users.noreply.github.com>
2024-11-16 14:27:24 +0700

    16/11

--- Project2/routes/web.php
+++ Project2/routes/web.php
@@ -22,13 +22,9 @@ Route::get('/fogotpassword', function () {
 Route::get('/accountcreation', function () {
     return view('accountcreation');
 });
-<<<<<<< HEAD
 Route::get('/showReset',[loginController::class,'showReset'] )->name('showReset');
 Route::get('/otp',[loginController::class,'sendResetCode'])->name('otp');
 Route::get('/resetPassword',[loginController::class,'resetPassword'])->name('resetPassword');
-=======
-Route::get('/resetPassword',[loginController::class,'showReset']);
-Route::post('/reset-password', [ResetPasswordController::class, 'handleReset'])->name('reset-password');
->>>>>>> b862c1c4780a94e3f6b76510c84f2d9c756c6488
+
 
 
