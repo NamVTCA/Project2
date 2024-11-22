839a2d4d1077a6b5de361afcc787aaf9e72aa020
Kratos17122004 <nguyenthanhquang.21062005@gmail.com>
2024-11-22 17:15:54 +0700

    Canh lề

--- Project2/app/Http/Controllers/loginController.php
+++ Project2/app/Http/Controllers/loginController.php
@@ -14,70 +14,70 @@ class loginController extends Controller
     function admin(){ return view('admin/dashboardadmin'); } 
     function teacher(){ return view('teacher/dashboardteacher'); } 
     function user(){ return view('user/dashboarduser'); }
-
-
-      public function showFogot(){
-        return view('forgotpassword');
-    }
-    function showLogin(){
-        return view('login');
-    }
-  
-    public function login(Request $request){
-    $request->validate([
-        'phone' => 'required',
-        'password' => 'required',
-    ]);
-    $identifier = $request->input('phone');
     
-    if (filter_var($identifier, FILTER_VALIDATE_EMAIL)) {
-        $user = User::where('email', $identifier)->first();
-    } else {
-        $user = User::where('phone', $identifier)->first();
-    }
+    public function showFogot(){ return view('forgotpassword');}
+    function showLogin(){ return view('login');}
+  
+    public function login(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required',
+            'password' => 'required',
+        ]);
+        $identifier = $request->input('phone');
+        
+        if (filter_var($identifier, FILTER_VALIDATE_EMAIL)) 
+        {
+            $user = User::where('email', $identifier)->first();
+        } 
+        else 
+        {
+            $user = User::where('phone', $identifier)->first();
+        }
 
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        return redirect()->route('showlogin')->with('message','invalid phone or email');
-    }
+        if (!$user || !Hash::check($request->password, $user->password)) 
+        {
+            return redirect()->route('showlogin')->with('message','invalid phone or email');
+        }
+        
         Auth::login($user);
 
-    $role = $user->role;
+        $role = $user->role;
 
-    switch ($role) {
-        case 0:
-            return redirect()->route('admin');
-        case 1:
-            return redirect()->route('teacher');
-        case 2:
-            return redirect()->route('user');
-        default:
-            return redirect()->route('user');
+        switch ($role) 
+        {
+            case 0:
+                return redirect()->route('admin');
+            case 1:
+                return redirect()->route('teacher');
+            case 2:
+                return redirect()->route('user');
+            default:
+                return redirect()->route('user');
+        }
     }
-    
-}
-public function logout(Request $request)
-{
- 
-    Auth::logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    return redirect()->route('showlogin')->with('message', 'Bạn đã đăng xuất thành công.');
-}
+        return redirect()->route('showlogin')->with('message', 'Bạn đã đăng xuất thành công.');
+    }
 
-
-public function sendResetCode(Request $request)
-{
-    $request->validate(['phone' => 'required']);
-    $phone = $request->input('phone');
-    $user = User::where('phone', $phone)->first();
-    
-    if (!$user) {
-        return back()->withErrors(['phone' => 'Số điện thoại không tồn tại']);
-    } 
+    public function sendResetCode(Request $request)
+    {
+        $request->validate(['phone' => 'required']);
+        $phone = $request->input('phone');
+        $user = User::where('phone', $phone)->first();
+        
+        if (!$user) 
+        {
+            return back()->withErrors(['phone' => 'Số điện thoại không tồn tại']);
+        } 
         $resetCode = Str::random(6);
         Session::put('reset_code', $resetCode);
-         Session::put('phone', $phone);
+        Session::put('phone', $phone);
         // Gửi mã OTP qua email
         Mail::raw("Mã xác nhận để đặt lại mật khẩu của bạn là: $resetCode", function ($message) use ($user) {
             $message->to($user->email);
@@ -87,38 +87,33 @@ public function sendResetCode(Request $request)
         // Thay vì sử dụng redirect()->route, dùng session flash để thông báo
         session()->flash('message', 'Mã xác nhận đã được gửi');
         
-         return redirect()->route('showfogot')->withInput(['phone' => $phone]);
-}
-
-
-public function resetPassword(Request $request)
-{
-    $request->validate([
-        'otp' => 'required|string',
-        'phone' => 'required|string',
-        'new_password' => 'required|string|min:8|same:confirm_password',
-    ]);
-
-    $codeInput = $request->input('otp');
-    $phone = $request->input('phone');
-    $user = User::where('phone', $phone)->first();
-
-    if (!$user) {
-        return response()->json(['success' => false, 'message' => 'Số điện thoại không tồn tại.']);
+        return redirect()->route('showfogot')->withInput(['phone' => $phone]);
     }
 
-    if ($codeInput != Session::get('reset_code')) {
-        return response()->json(['success' => false, 'message' => 'Mã xác nhận không hợp lệ.']);
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'otp' => 'required|string',
+            'phone' => 'required|string',
+            'new_password' => 'required|string|min:8|same:confirm_password',
+        ]);
+
+        $codeInput = $request->input('otp');
+        $phone = $request->input('phone');
+        $user = User::where('phone', $phone)->first();
+
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'Số điện thoại không tồn tại.']);
+        }
+
+        if ($codeInput != Session::get('reset_code')) {
+            return response()->json(['success' => false, 'message' => 'Mã xác nhận không hợp lệ.']);
+        }
+
+        $user->password = Hash::make($request->input('new_password'));
+        $user->save();
+
+        return redirect()->route('showfogot');
     }
-
-    $user->password = Hash::make($request->input('new_password'));
-    $user->save();
-
-    return redirect()->route('showfogot');
 }
-}
-
-
-
-
-
--- Project2/app/Http/Controllers/paymentController.php
+++ Project2/app/Http/Controllers/paymentController.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Mail;
 
 class paymentController extends Controller
 {
-    
     function index(){
         $children = Child::all();
         $tuitions = tuition::with('tuition_info')->get();
@@ -36,7 +35,8 @@ class paymentController extends Controller
 
         $result = curl_exec($ch);
 
-        if(curl_errno($ch)) {
+        if(curl_errno($ch)) 
+        {
             Log::error('Lỗi Curl: ' . curl_error($ch));
         }
         
@@ -44,96 +44,98 @@ class paymentController extends Controller
         return $result;
     }
 
-      public function momo_payment(Request $request)
-{
-    try {
-        $tuition = tuition::with('tuition_info')->find($request->tuition_id);
+    public function momo_payment(Request $request)
+    {
+        try 
+        {
+            $tuition = tuition::with('tuition_info')->find($request->tuition_id);
 
-        if (!$tuition) {
-            return response()->json(['error' => 'Không tìm thấy thông tin học phí'], 404);
-        }
+            if (!$tuition) 
+            {
+                return response()->json(['error' => 'Không tìm thấy thông tin học phí'], 404);
+            }
 
-        $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
-        $partnerCode = 'MOMOBKUN20180529';
-        $accessKey = 'klm05TvNBzhg7h7j';
-        $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
-        $amount = (string) intval($tuition->tuition_info->sum('price'));
-        $orderId = time() . "";
-        $orderInfo = "Thanh toán học phí kỳ " . $tuition->semester;
-        $redirectUrl = route('momo');
-        $ipnUrl = "http://localhost:8000/atm/ipn_momo.php";
-        $requestId = $orderId;
-        $requestType = "payWithATM";
-        $extraData = "";
-        $rawHash = "accessKey=" . $accessKey . 
-                  "&amount=" . $amount . 
-                  "&extraData=" . $extraData . 
-                  "&ipnUrl=" . $ipnUrl . 
-                  "&orderId=" . $orderId . 
-                  "&orderInfo=" . $orderInfo . 
-                  "&partnerCode=" . $partnerCode . 
-                  "&redirectUrl=" . $redirectUrl . 
-                  "&requestId=" . $requestId . 
-                  "&requestType=" . $requestType;
-        $signature = hash_hmac("sha256", $rawHash, $secretKey);
+            $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+            $partnerCode = 'MOMOBKUN20180529';
+            $accessKey = 'klm05TvNBzhg7h7j';
+            $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+            $amount = (string) intval($tuition->tuition_info->sum('price'));
+            $orderId = time() . "";
+            $orderInfo = "Thanh toán học phí kỳ " . $tuition->semester;
+            $redirectUrl = route('momo');
+            $ipnUrl = "http://localhost:8000/atm/ipn_momo.php";
+            $requestId = $orderId;
+            $requestType = "payWithATM";
+            $extraData = "";
+            $rawHash = "accessKey=" . $accessKey . 
+                    "&amount=" . $amount . 
+                    "&extraData=" . $extraData . 
+                    "&ipnUrl=" . $ipnUrl . 
+                    "&orderId=" . $orderId . 
+                    "&orderInfo=" . $orderInfo . 
+                    "&partnerCode=" . $partnerCode . 
+                    "&redirectUrl=" . $redirectUrl . 
+                    "&requestId=" . $requestId . 
+                    "&requestType=" . $requestType;
+            $signature = hash_hmac("sha256", $rawHash, $secretKey);
 
-        $data = array(
-            'partnerCode' => $partnerCode,
-            'partnerName' => "Test",
-            'storeId' => "MomoTestStore",
-            'requestId' => $requestId,
-            'amount' => $amount,
-            'orderId' => $orderId,
-            'orderInfo' => $orderInfo,
-            'redirectUrl' => $redirectUrl,
-            'ipnUrl' => $ipnUrl,
-            'lang' => 'vi',
-            'extraData' => $extraData,
-            'requestType' => $requestType,
-            'signature' => $signature
-        );
+            $data = array(
+                'partnerCode' => $partnerCode,
+                'partnerName' => "Test",
+                'storeId' => "MomoTestStore",
+                'requestId' => $requestId,
+                'amount' => $amount,
+                'orderId' => $orderId,
+                'orderInfo' => $orderInfo,
+                'redirectUrl' => $redirectUrl,
+                'ipnUrl' => $ipnUrl,
+                'lang' => 'vi',
+                'extraData' => $extraData,
+                'requestType' => $requestType,
+                'signature' => $signature
+            );
 
-        Log::info('Yêu cầu thanh toán MoMo:', $data);
+            Log::info('Yêu cầu thanh toán MoMo:', $data);
 
-        $result = $this->execPostRequest($endpoint, json_encode($data));
-        Log::info('Phản hồi thanh toán MoMo:', ['response' => $result]);
+            $result = $this->execPostRequest($endpoint, json_encode($data));
+            Log::info('Phản hồi thanh toán MoMo:', ['response' => $result]);
 
-        $jsonResult = json_decode($result, true);
-        if (isset($jsonResult['payUrl'])) {
-            $tuition->update(['status' => 1]);
-            session(['momo_payment_order_id' => $orderId]);
-            $child = child::with('tuition')->find($tuition->child_id);
-            $user = User::find($child->user_id);
-            $userEmail = $user->email;
-            Mail::raw("tong so tien de thanh toan la:  $amount", function ($message) use ($user) {
-            $message->to($user->email);
-            $message->subject('Hóa đơn thanh toán học phí');
-            });
-            return redirect($jsonResult['payUrl']);
-        }
-        
-        Log::error('Lỗi thanh toán MoMo:', $jsonResult);
-        return response()->json([
-            'error' => $jsonResult['message'] ?? 'Lỗi không xác định',
-            'details' => $jsonResult
-        ], 400);
-
-    } catch (\Exception $e) {
-        Log::error('Ngoại lệ thanh toán MoMo:', ['error' => $e->getMessage()]);
-        return response()->json(['error' => 'Đã xảy ra lỗi trong quá trình xử lý thanh toán'], 500);
+            $jsonResult = json_decode($result, true);
+            if (isset($jsonResult['payUrl'])) {
+                $tuition->update(['status' => 1]);
+                session(['momo_payment_order_id' => $orderId]);
+                $child = child::with('tuition')->find($tuition->child_id);
+                $user = User::find($child->user_id);
+                $userEmail = $user->email;
+                Mail::raw("tong so tien de thanh toan la:  $amount", function ($message) use ($user) {
+                $message->to($user->email);
+                $message->subject('Hóa đơn thanh toán học phí');
+                });
+                return redirect($jsonResult['payUrl']);
+            }
+            
+            Log::error('Lỗi thanh toán MoMo:', $jsonResult);
+            return response()->json([
+                'error' => $jsonResult['message'] ?? 'Lỗi không xác định',
+                'details' => $jsonResult
+            ], 400);
+        } 
+        catch (\Exception $e) 
+        {
+            Log::error('Ngoại lệ thanh toán MoMo:', ['error' => $e->getMessage()]);
+            return response()->json(['error' => 'Đã xảy ra lỗi trong quá trình xử lý thanh toán'], 500);
+        }  
     }
-    
-}
 
-public function getTuitionsByChild($childId)
-{
-    $tuitions = tuition::where('child_id', $childId)->with('tuition_info')->get();
-    return response()->json($tuitions);
-}
-public function getTuitionDetails($tuitionId)
-{
-    $details = tuition_info::where('tuition_id', $tuitionId)->get();
-    return response()->json($details);
-}
+    public function getTuitionsByChild($childId)
+    {
+        $tuitions = tuition::where('child_id', $childId)->with('tuition_info')->get();
+        return response()->json($tuitions);
+    }
 
+    public function getTuitionDetails($tuitionId)
+    {
+        $details = tuition_info::where('tuition_id', $tuitionId)->get();
+        return response()->json($details);
+    }
 }
