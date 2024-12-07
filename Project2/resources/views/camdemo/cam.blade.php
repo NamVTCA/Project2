7fb177f64a77e3b40f2f7162a56e570b9004ca75
truongbvnedu <169114325+truongbvnedu@users.noreply.github.com>
2024-12-07 19:35:41 +0700

    70% function watch cam

--- Project2/resources/views/camdemo/cam.blade.php
+++ Project2/resources/views/camdemo/cam.blade.php
@@ -1,16 +1,11 @@
-
 <div class="container">
     <h1>Danh sách camera</h1>
     <div class="row">
         @foreach($cameras as $camera)
             <div class="col-md-4">
                 <h3>{{ $camera->name }}</h3>
-                <video width="100%" controls autoplay>
-                    <source src="{{ $camera->stream_url }}" type="application/x-mpegURL">
-                    Trình duyệt không hỗ trợ video trực tiếp.
-                </video>
+                <iframe src="{{ $camera->stream_url }}" width="100%" height="500" frameborder="0" allowfullscreen></iframe>
             </div>
         @endforeach
     </div>
 </div>
-
